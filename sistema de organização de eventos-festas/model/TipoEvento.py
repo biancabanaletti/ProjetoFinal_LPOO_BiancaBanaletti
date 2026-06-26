2671d567b61cb59c7f78b778d1068073176eab6a
biancabanaletti <biancabanaletti.pf009@academico.ifsul.edu.br>
2026-06-26 15:46:27 -0300

    Delete sistema de organização de eventos-festas directory

--- sistema de organização de eventos-festas/model/TipoEvento.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class TipoEvento(Enum):
-
-    CASAMENTO = "casamento"
-    FESTA_INFANTIL = "festa infantil"
-    EVENTO_CORPORATIVO = "evento corporativo"
-    FORMATURA = "formatura"
-    ANIVERSARIO = "aniversario"
